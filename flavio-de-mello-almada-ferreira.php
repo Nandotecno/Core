35d67355a2801f78ffa54c21f84f3f5524016ca6
Fernando Cruz <86312129+Nandotecno@users.noreply.github.com>
2022-06-08 20:28:12 -0300

    Add files via upload

---
+++ flavio-de-mello-almada-ferreira.php
@@ -0,0 +1,25 @@
+<div class="cab" style="background: url('../assets/img/headers/sobre-bg.jpg') no-repeat;">
+    <div class="text-center">
+        <h2>Our Team</h2>
+    </div>
+</div>
+
+<section class="inner-page">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <h2>Flávio de Mello Almada Ferreira</h2>
+                <hr>
+                <strong>Sócio do escritório Sá e Ferreira Advogados Associados</strong>
+                <ul>
+                  <li>Graduated from the Pontifical Catholic University of São Paulo (PUC-SP) - 2016</li>
+                  <li>Specialization - Digital / Electronic Law - FGV Law - 2018</li>
+                  <li>Legal English Advanced – Thomas Jefferson School of Law – 2019</li>
+                  <li>English and Spanish</li>
+            </div>
+            <div class="col-md-4">
+                <img src="../assets/img/team/dr-flavio-ferreira-e-sa.jpg" alt="Flávio de Mello Almada Ferreira" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
