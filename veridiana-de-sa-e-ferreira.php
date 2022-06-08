2ea076421c210e5123cf7568ce6f711831dc215a
Fernando Cruz <86312129+Nandotecno@users.noreply.github.com>
2022-06-08 20:34:18 -0300

    Add files via upload

---
+++ veridiana-de-sa-e-ferreira.php
@@ -0,0 +1,26 @@
+<div class="cab" style="background: url('../assets/img/headers/sobre-bg.jpg') no-repeat;">
+    <div class="text-center">
+        <h2>Our Team</h2>
+    </div>
+</div>
+
+<section class="inner-page">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <h2>Veridiana Machado de Sá e Ferreira</h2>
+                <hr>
+                <strong>Sócia do escritório Sá e Ferreira Advogados Associados
+                </strong>
+                <ul>
+                  <li>Graduated in Law from the Catholic Faculty of Law of Santos (UNISANTOS) - 1996</li>
+                  <li>Post-graduation Lato Sensu - Juridical and Social Sciences - UNISANTOS - 1997</li>
+                  <li>Specialization in Civil Liability by Fundação Getúlio Vargas - (FGV Law) - 2010</li>
+                  <li>English and Spanish</li>
+            </div>
+            <div class="col-md-4">
+                <img src="../assets/img/team/dra-veridiana-ferreira-e-sa.jpg" alt="Veridiana Machado de Sá e Ferreira" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
