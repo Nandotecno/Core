cf9643fe4e67aaea8e50710d84f547c640b37f89
Fernando Cruz <86312129+Nandotecno@users.noreply.github.com>
2022-06-08 20:29:51 -0300

    Add files via upload

---
+++ alexandre-ferreira.php
@@ -0,0 +1,28 @@
+<div class="cab" style="background: url('../assets/img/headers/sobre-bg.jpg') no-repeat;">
+    <div class="text-center">
+        <h2>Our Team</h2>
+    </div>
+</div>
+<section class="inner-page">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <h2>Alexandre Ferreira</h2>
+                <hr>
+                <strong>Office Partner Sá and Ferreira Associated Lawyers
+                </strong>
+                <ul>
+                    <li>Graduated from the Catholic University of Santos (UNISANTOS) – 1990</li>
+                    <li>Postgraduate Lato Sensu - Real Estate Right Applied and Real Estate Real Estate - FGV Law (FGV Law) – 2013</li>
+                    <li>Specialization - Digital / Electronic Law - FGV Law - 2018 </ li>
+                    <li>Prosecutor of the Anti-Doping Sports Court in Brasília (DF) - APR/2017 to SEP/2018</li>
+                    <li>Auditor of the Anti-Doping Sports Court in Brasilia (DF) </li>
+                    <li>English and Spanish</li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <img src="../assets/img/team/dr-alexandre-ferreira-e-sa.jpg" alt="Alexandre Ferreira" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
